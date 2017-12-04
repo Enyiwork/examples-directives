b41cab5ce087356c1ff85a0380db7833c56b1ed5
Enyiwork <32691424+Enyiwork@users.noreply.github.com>
2017-12-04 14:56:30 -0500

    examples of directives

---
+++ src/app/animal-list/animals.ts
@@ -0,0 +1,32 @@
+export const animalList: any[] = [
+  {
+    id: 1,
+    name: "Dinosaur",
+    imageUrl: "https://media.giphy.com/media/l1Kui8t10bD5h3Hbi/giphy.gif"
+  },
+  {
+    id: 5,
+    name: "Platypus",
+    imageUrl: "https://media.giphy.com/media/11MMdMaNxcrDyw/giphy.gif"
+  },
+  {
+    id: 15,
+    name: "blue-footed Booby",
+    imageUrl: "https://media.giphy.com/media/ChvWFAwo16FqM/giphy.gif"
+  },
+  {
+    id: 20,
+    name: "Sloth",
+    imageUrl: "https://media.giphy.com/media/rdbyJJX8NbSBW/giphy.gif"
+  },
+  {
+    id: 25,
+    name: "Prooboscis Monky",
+    imageUrl: "https://media.giphy.com/media/qPZ64DDFAUQU0/giphy.gif"
+  },
+  {
+    id: 30,
+    name: "Liger",
+    imageUrl: "https://media.giphy.com/media/pBl6VfAvgo04E/giphy.gif"
+  },
+]
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NumberInputComponent } from './number-input/number-input.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NumberInputComponent,
+    AnimalListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
