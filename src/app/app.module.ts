2423c331d6a2559d66c49d09136d3d3cba9cd859
Enyiwork <32691424+Enyiwork@users.noreply.github.com>
2017-12-04 16:39:51 -0500

    examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NumberInputComponent } from './number-input/number-input.component';
 import { AnimalListComponent } from './animal-list/animal-list.component';
+import { TravelSelectionComponent } from './travel-selection/travel-selection.component';
+import { TextOnfiguratorComponent } from './text-onfigurator/text-onfigurator.component';
+import { TextConfiguratorComponent } from './text-configurator/text-configurator.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NumberInputComponent,
-    AnimalListComponent
+    AnimalListComponent,
+    TravelSelectionComponent,
+    TextOnfiguratorComponent,
+    TextConfiguratorComponent
   ],
   imports: [
     BrowserModule,
